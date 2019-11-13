949ec8bf4cdac41ad684392d9ec2cab31f59bf79
BIT <BIT@DESKTOP-8KOUOA5>
2019-11-13 19:00:29 +0900

    20191113

--- src/practice01/Prob05.java
+++ src/practice01/Prob05.java
@@ -10,7 +10,27 @@ public class Prob05 {
 //		char c = strNumber.charAt(0);
 //		System.out.println(c); //9
 	
-		
+		for(int n = 1; n < 100; n++) {
+			System.out.print(n + "\t");
+			
+			String str = String.valueOf(n);
+			
+			for(int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				
+				switch(c) {
+				case '3':
+				case '6':
+				case '9':
+					System.out.print("짝");
+					break;
+				default:
+					break;
+				}
+				
+			}
+			System.out.println();
+		}
 	}
 
 }
